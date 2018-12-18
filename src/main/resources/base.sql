1a34348a367061668657401198b758c17ef061ef
Sergio Viera <sergioviera@gmail.com>
2018-12-18 19:12:33 -0300

    corregir base de datos

--- src/main/resources/base.sql
+++ src/main/resources/base.sql
@@ -1,17 +1,84 @@
 CREATE DATABASE idb;
 
-CREATE TABLE `ibd`.`files` (
-  `id` BIGINT(20) NOT NULL,
-  `url` VARCHAR(1000) NULL,
-  `fecha_creado` TIMESTAMP NULL,
-  `id_investigacion` BIGINT(20) NULL,
-  `id_usuario` BIGINT(20) NULL,
-  PRIMARY KEY (`id`));
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-12-2018 a las 23:11:09
+-- Versión del servidor: 10.1.22-MariaDB
+-- Versión de PHP: 7.1.4
 
-CREATE TABLE `ibd`.`steps` (
-  `id` BIGINT(20) NOT NULL,
-  `url` VARCHAR(1000) NULL,
-  `fecha_creado` TIMESTAMP NULL,
-  `id_investigacion` BIGINT(20) NULL,
-  `id_usuario` BIGINT(20) NULL,
-  PRIMARY KEY (`id`));
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `ibd`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `files`
+--
+
+CREATE TABLE `files` (
+  `id` bigint(20) NOT NULL,
+  `nombre_archivo` varchar(255) NOT NULL,
+  `id_investigacion` bigint(20) DEFAULT NULL,
+  `id_usuario` bigint(20) DEFAULT NULL,
+  `estado` int(10) NOT NULL,
+  `fecha_subido` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `steps`
+--
+
+CREATE TABLE `steps` (
+  `id` bigint(20) NOT NULL,
+  `url` varchar(1000) DEFAULT NULL,
+  `fecha_creado` timestamp NULL DEFAULT NULL,
+  `id_investigacion` bigint(20) DEFAULT NULL,
+  `id_usuario` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `steps`
+--
+ALTER TABLE `steps`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
