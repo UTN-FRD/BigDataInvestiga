a1c57481ce1a4cfe22f5226640ff842b3f4c9e36
Sergio Viera <sergioviera@gmail.com>
2019-06-11 17:39:18 -0300

    guardar paso en RESTful

--- src/main/webapp/filter/managers/Manager.jquery.js
+++ src/main/webapp/filter/managers/Manager.jquery.js
@@ -34,16 +34,19 @@ AjaxSolr.Manager = AjaxSolr.AbstractManager.extend(
       options.url = this.solrUrl + servlet + '?' + string + '&wt=json' + (disableJsonp ? '' : '&json.wrf=?');
     }
     
+    //llamada para guardar el paso
     var params = {};
     params.url = options.url;
     params.idUsuario = 1; //CAMBIAR
     params.idInvestigacion = window.location.hash.substr(1);
+    params.fecha = (new Date()).toLocaleDateString();
     $.ajax({
-      url: '../ajax/savestep',
-      data: params,
+      url: BASEURL+'savestep'+ENDURL,
+      data: JSON.stringify(params),
       type: 'POST'
     });
     
+    //llamada a SOLR
     jQuery.ajax(options).done(handler).fail(errorHandler);
   }
 });
